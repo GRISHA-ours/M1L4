4cb7607a75217ebb2bb7ca97cf69e96946558826
Taksist Ashot <grigory260511@yandex.ru>
2025-07-24 20:30:35 +0200

    кормление поекемонов

--- logic.py
+++ logic.py
@@ -1,17 +1,19 @@
 from random import randint
 import requests
-
+from datetime import timedelta,datetime
 class Pokemon:
     pokemons = {}
     # Инициализация объекта (конструктор)
     def __init__(self, pokemon_trainer):
 
         self.pokemon_trainer = pokemon_trainer   
-
-        self.pokemon_number = randint(1,1302)
+        self.last_feed_time = datetime.now
+        self.pokemon_number = randint(1,1000)
         self.img = self.get_img()
         self.name = self.get_name()
         self.ability = self.get_ability()
+        self.hp = randint(500,17500)
+        self.power = randint(1000,3500)
         Pokemon.pokemons[pokemon_trainer] = self
 
     # Метод для получения картинки покемона через API
@@ -23,7 +25,30 @@ class Pokemon:
             return (data['sprites']["front_default"])
         else:
             return "Pikachu"
-    
+
+
+    def feed(self, feed_interval = 20, hp_increase = 10 ):
+        current_time = datetime.now()  
+        delta_time = timedelta(seconds=feed_interval)  
+        if (current_time - self.last_feed_time) > delta_time:
+            self.hp += hp_increase
+            self.last_feed_time = current_time
+            return f"Здоровье покемона увеличено. Текущее здоровье: {self.hp}"
+        else:
+            return f"Следующее время кормления покемона: {current_time+delta_time}"
+
+
+
+
+
+    def attack(self, enemy):
+        if enemy.hp > self.power:
+            enemy.hp -= self.power
+            return f"Сражение @{self.pokemon_trainer} с @{enemy.pokemon_trainer} осталось здоровья: {enemy.hp}"
+        else:
+            enemy.hp = 0
+            return f"Победа @{self.pokemon_trainer} над @{enemy.pokemon_trainer}! "
+
     # Метод для получения имени покемона через API
     def get_name(self):
         url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
@@ -49,11 +74,23 @@ class Pokemon:
 
     # Метод класса для получения информации
     def info(self):
-        return f"Имя твоего покеомона: {self.name} способность:{self.ability}"
+        return f"Имя твоего покеомона: {self.name} способность:{self.ability} здоровье: {self.hp} урон: {self.power}"
 
     # Метод класса для получения картинки покемона
     def show_img(self):
         return self.img
 
+
+class Fighter(Pokemon):
+
+    def __init__(self, pokemon_trainer):
+        super().__init__(pokemon_trainer)
+        self.power += randint(3000, 6000)
+
+class Wizard(Pokemon):
+
+    def __init__(self, pokemon_trainer):
+        super().__init__(pokemon_trainer)
+        self.hp += randint(5000, 22000)
 
 
